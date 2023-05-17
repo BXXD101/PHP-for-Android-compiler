080123a9f3cef9f3d10d4e997aadb9b491e0c437
BXXD101 <aytwmur@gmail.com>
2023-05-17 23:46:34 +0200

    Update compileandroid.sh

--- compileandroid.sh
+++ compileandroid.sh
@@ -4,11 +4,6 @@ echo "Downloading toolchains"
 wget --quiet http://musl.cc/aarch64-linux-musl-cross.tgz
 tar -xzf aarch64-linux-musl-cross.tgz
 
-sqlite=`pwd`/sql
-libxml=`pwd`/xml
-zlib=`pwd`/lib
-# zip=`pwd`/zip we will add zip support later
-
 git clone https://github.com/php/php-src
 echo "Creating config.."
 `pwd`/php-src/buildconf
@@ -23,24 +18,16 @@ export LD=$TOOLCHAIN/aarch64-linux-musl-ld
 export RANLIB=$TOOLCHAIN/aarch64-linux-musl-ranlib
 export STRIP=$TOOLCHAIN/aarch64-linux-musl-strip
 
-#wget --quiet https://libzip.org/download/libzip-1.9.2.tar.gz
-#tar -xzf libzip-1.9.2.tar.gz
-#cd libzip-1.9.2
-#cmake . -DBUILD_SHARED_LIBS=OFF -DCMAKE_PREFIX_PATH=$zip -DCMAKE_INSTALL_PREFIX=$zip -DCMAKE_INSTALL_LIBDIR=lib -DBUILD_TOOLS=OFF -DBUILD_REGRESS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_DOC=OFF -DENABLE_BZIP2=OFF -DENABLE_COMMONCRYPTO=OFF -DENABLE_GNUTLS=OFF -DENABLE_MBEDTLS=OFF -DENABLE_LZMA=OFF
-#make -j$(nproc)
-#make install
-#cd ..
-
 git clone https://github.com/sqlite/sqlite
 cd sqlite
-./configure --enable-static=yes --prefix=$sql --host=$TARGET --enable-shared=no --disable-dependency-tracking --enable-static-shell=no 
+./configure --enable-static=yes --host=$TARGET --enable-shared=no --disable-dependency-tracking --enable-static-shell=no 
 make -j$(nproc)
 make install
 cd ..
 
 git clone https://github.com/madler/zlib
 cd zlib
-./configure --prefix=$lib --static
+./configure --static
 make -j$(nproc)
 make install
 cd ..
@@ -48,13 +35,10 @@ cd ..
 wget --quiet https://gitlab.gnome.org/GNOME/libxml2/-/archive/v2.10.1/libxml2-v2.10.1.tar.gz
 tar -xzf libxml2-v2.10.1.tar.gz
 cd libxml2-v2.10.1
-./autogen.sh --enable-static=yes --prefix=$xml --host=$TARGET --without-lzma --without-iconv --without-python --enable-shared=no --with-zlib=$lib
+./autogen.sh --enable-static=yes --host=$TARGET --without-lzma --without-iconv --without-python --enable-shared=no --with-zlib
 make -j$(nproc)
 make install
 cd ..
-
-git clone https://github.com/pmmp/PocketMine-C-ChunkUtils
-mv PocketMine-C-ChunkUtils php-src/ext
 
 cd php-src
 ./configure CFLAGS="-static" --host=$TARGET \
@@ -62,10 +46,9 @@ cd php-src
 --enable-pocketmine-chunkutils
 --enable-ipv6 \
 --enable-static \
---with-sqlite=$sql
---with-zlib=$lib \
+--with-zlib \
 --without-iconv \
---with-libxml=$xml \
+--with-libxml \
 --disable-opcache \
 --disable-shared
 
